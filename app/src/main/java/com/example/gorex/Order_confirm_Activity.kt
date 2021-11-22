7e793479990ebaf8f7d998712fbba7afc328433c
adnanakhtar2000 <adnanakhtar.10001@gmail.com>
2021-11-22 13:48:31 +0500

    Update Project

---
+++ app/src/main/java/com/example/gorex/Order_confirm_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.gorex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Order_confirm_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_confirm)
+    }
+}
